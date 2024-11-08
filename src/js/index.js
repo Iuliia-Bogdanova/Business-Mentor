9e5f0a2107c9d776727c2290efaf3d8f62cd8be9
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2024-11-08 10:59:47 +0400

    feat: add modules - button.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,26 @@
+import buttons from "./modules/button.js";
+
+buttons();
+
 // import mobileNav from './modules/mobile-nav.js';
 // mobileNav();
 
-const buttons = document.querySelectorAll(".select");
+// const buttons = document.querySelectorAll(".select");
 
-buttons.forEach((button) => {
-    button.addEventListener("click", function () {
+// buttons.forEach((button) => {
+//     button.addEventListener("click", function () {
 
-        buttons.forEach((btn) => {
-            if (btn !== button) {
-                btn.classList.add("unclickable"); 
-            }
-        });
+//         buttons.forEach((btn) => {
+//             if (btn !== button) {
+//                 btn.classList.add("unclickable");
+//             }
+//         });
 
-        button.disabled = true;
+//         button.disabled = true;
 
-        const card = button.closest(".card");
-        const cardTitle = card.querySelector(".card__title").textContent;
+//         const card = button.closest(".card");
+//         const cardTitle = card.querySelector(".card__title").textContent;
 
-        console.log("Вы выбрали тариф: " + cardTitle);
-    });
-});
-
+//         console.log("Вы выбрали тариф: " + cardTitle);
+//     });
+// });
---
+++ src/js/modules/button.js
@@ -0,0 +1,22 @@
+function buttons() {
+    const buttons = document.querySelectorAll(".select");
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", function () {
+            buttons.forEach((btn) => {
+                if (btn !== button) {
+                    btn.classList.add("unclickable");
+                }
+            });
+
+            button.disabled = true;
+
+            const card = button.closest(".card");
+            const cardTitle = card.querySelector(".card__title").textContent;
+
+            console.log("Вы выбрали тариф: " + cardTitle);
+        });
+    });
+}
+
+export default buttons;
